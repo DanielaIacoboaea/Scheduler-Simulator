fc896977dfc5612d1ff01e4a7d0bfd56c7b6bba9
Daniela <iacoboaea@gmail.com>
2022-12-02 17:57:44 +0000

    Remove react components from backend folder

--- simulation/scheduler/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("scheduler", views.scheduler, name="scheduler"),
-    path("prefilled/<str:name>/<int:type>", views.prefilled_scheduler, name="prefilled")
-]
